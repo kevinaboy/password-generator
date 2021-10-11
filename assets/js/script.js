33d807b2261b206b9967b222b199b19af603212b
School <kaboy@icloud.com>
2021-10-10 23:40:53 -0500

    add app screenshot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,38 @@
 // Assignment code here
 
+// DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const clipboardEl = document.getElementById('clipboard');
+
+const randomFun = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+// General Functions
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}-=,./<>';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +48,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  const hasUpper = uppercaseEl.checked;
+
+  console.log(hasUpper);
+/*
+generateBtn.addEventListener("click", ()=> {
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  console.log(hasLower, hasUpper, hasNumber, hasSymbol);
+
+});
+
+*/
+
+/*original code for generate button
+
+generateBtn.addEventListener("click", writePassword);
+
+*/
